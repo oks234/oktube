5932328714f28e21bdf20ccc50d2507021e552ae
oks234 <bestspx@gmail.com>
2022-02-11 21:56:11 +0900

    #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
-}
-
-const handleHome = (req, res, next) => {
-  return res.end("I love middlewares");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+app.get("/", logger, handleHome);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
