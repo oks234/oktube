bd01986cadf4a8dedea6ac7804c17df188d74110
oks234 <bestspx@gmail.com>
2022-02-12 18:05:42 +0900

    #3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,17 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const routerLogger = (req, res, next) => {
-  console.log("PATH", req.path);
-  next();
-};
-const methodLogger = (req, res, next) => {
-  console.log("METHOD", req.method);
-  next();
-};
+const logger = morgan("dev");
 
 const home = (req, res) => {
   return res.send("hello");
@@ -20,7 +13,7 @@ const login = (req, res) => {
   return res.send("login");
 };
 
-app.use(methodLogger, routerLogger);
+app.use(logger);
 
 app.get("/", home);
 app.get("/login", login);
