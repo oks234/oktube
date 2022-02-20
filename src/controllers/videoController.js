35422ca3132caf9a9a872d5df04a4dc1979d64f1
oks234 <bestspx@gmail.com>
2022-02-20 11:37:38 +0900

    #4.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => res.send("Home Page Vidoes");
-export const see = (req, res) => res.send("See Video");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send(`Watch video #${req.params.id}`);
+};
 export const edit = (req, res) => res.send("Edit Video");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
