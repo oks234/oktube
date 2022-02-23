8411cb3a60b4b01632394712151405847676d725
oks234 <bestspx@gmail.com>
2022-02-23 22:36:12 +0900

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,21 @@
+const fakeUser = {
+  username: "Kyungseok",
+  loggedIn: false,
+};
+
 export const trending = (req, res) =>
   res.render("home", {
     pageTitle: "Home",
+    fakeUser,
   });
-export const see = (req, res) => res.render("watch", {
-  pageTitle: "Watch",
-});
-export const edit = (req, res) => res.render("edit", {
-  pageTitle: "Edit",
-});
+export const see = (req, res) =>
+  res.render("watch", {
+    pageTitle: "Watch",
+  });
+export const edit = (req, res) =>
+  res.render("edit", {
+    pageTitle: "Edit",
+  });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
