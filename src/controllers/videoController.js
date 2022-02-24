12504cff76115c3a8b97e041f8aca6f4982958ad
oks234 <bestspx@gmail.com>
2022-02-24 23:51:42 +0900

    #6.0 Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,18 +1,23 @@
+let videos = [
+  { title: "My Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 1 },
+  { title: "Your Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 2 },
+  { title: "Our Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 3 },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    { title: "My Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 1 },
-    { title: "Your Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 1 },
-    { title: "Our Video", rating: 5, comments: 2, createAt: "2 minutes ago", views: 59, id: 1 },
-  ];
   return res.render("home", {
     pageTitle: "Home",
     videos,
   });
 };
-export const see = (req, res) =>
-  res.render("watch", {
-    pageTitle: "Watch",
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video,
   });
+};
 export const edit = (req, res) =>
   res.render("edit", {
     pageTitle: "Edit",
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { deleteVideo, edit, see, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
