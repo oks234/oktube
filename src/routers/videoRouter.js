741d20f25a8aeee72b67ab70c373e2e4ed3edecd
dean-oks <oks234@deancorporation.com>
2022-03-10 17:39:13 +0900

    #6.25 Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { getEdit, getUpload, postEdit, postUpload, watch } from "../controllers/videoController";
+import {
+  deleteVideo,
+  getEdit,
+  getUpload,
+  postEdit,
+  postUpload,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
