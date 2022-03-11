910fbaf85c3cd2345b15f6f4e68a0049cbbc9854
dean-oks <oks234@deancorporation.com>
2022-03-11 18:00:33 +0900

    #7.10 Logged In User part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -19,16 +20,7 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-});
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
