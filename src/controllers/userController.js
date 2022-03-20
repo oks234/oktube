486560907bec9ef52bea93a1d92cdde30f776613
dean-oks <oks234@deancorporation.com>
2022-03-20 15:43:26 +0900

    #7.18 Github Login part Three

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -61,18 +61,31 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "e5bc5695da64de5ad310",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
   const params = new URLSearchParams(config);
-  const url = `${baseUrl}?${params}`;
+  const finalUrl = `${baseUrl}?${params}`;
 
-  return res.redirect(url);
+  return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
-  
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    header: { Accept: "application / json" },
+  });
+  const json = await data.json();
+  console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit User");
